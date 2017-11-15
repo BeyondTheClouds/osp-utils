250c385945d4b8e97c51eadfbd555fd0cdb5b8b0
RFish ⚓ <RonanCherrueau@gmail.com>
2017-11-15 19:15:43 +0100

    towards sql interpretation

--- default.nix
+++ default.nix
@@ -1,12 +1,18 @@
-with import <nixpkgs> {}; {
-_ = let ghc = haskellPackages.ghcWithHoogle (pkgs: with pkgs; [
+with import <nixpkgs> {};
+
+let ghc = haskellPackages.ghcWithHoogle (pkgs: with pkgs; [
   # These are for spacemacs haskell layer. To get spacemacs with the
   # correct PATH. run nix-shell, then launch Emacs inside this
   # nix-shell.
-  apply-refact hlint stylish-haskell hasktags ghc-mod
+  apply-refact hlint stylish-haskell hasktags
+  # FIXME: ghc-syb-utils check fails because of a missing file.
+  # It comes from by ghc-mod. Remove it when check is fixed.
+  # ghc-syb-utils #.override (self: super: { doCheck = false; }) # used by ghc-mod
+  ghc-mod
 
   #nix-env -qaP -A nixos.haskellPackages|fgrep aeson
-  cabal-install aeson extra HUnit rosezipper
+  cabal-install aeson extra HUnit
+  simple-sql-parser
 ]);
 in stdenv.mkDerivation {
   name = "osp-utils";
@@ -14,5 +20,4 @@ in stdenv.mkDerivation {
   shellHook = ''
     eval $(egrep ^export ${ghc}/bin/ghc)
   '';
-};
 }
